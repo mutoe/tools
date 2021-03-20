9c81f1640966c16d2aa83cf7ede85585fe16bf96
mutoe <mutoe@foxmail.com>
2021-03-20 22:34:44 +0800

    chore: setup typescript

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>Mutoe's Tools</h1>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
